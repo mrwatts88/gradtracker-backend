9e8448a9f49b6b378fce53ed966d217154bc8686
Yubin <zheng24@uwm.edu>
2019-09-29 03:00:22 -0500

    Created fields, forms databases and models, Things still needs to do: create unit test for fields, forms, and Profile Service. Questions needs to ask: what is the definition of the "strong" password(e.g. 1 capatialized letter, 1 special letter, letter and number combinations)?

--- src/main/java/edu/uwm/capstone/service/ProfileService.java
+++ src/main/java/edu/uwm/capstone/service/ProfileService.java
@@ -1,11 +1,13 @@
 package edu.uwm.capstone.service;
 
+import edu.uwm.capstone.db.ProfileDao;
 import edu.uwm.capstone.model.Profile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import edu.uwm.capstone.sql.exception.DaoException;
 
 @Service("profileService")
 public class ProfileService {
@@ -14,15 +16,18 @@ public class ProfileService {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+    private ProfileDao profileDao;
 
     public Profile create(Profile profile) {
-
-        // TODO check if email is legit
-        //  check if email already exists in db,
-        //  check if password is strong
+        //  check if email is legit (done)
+        //  check if email already exists in db, (done)
+        // TODO check if password is strong (define strong?)
+        if(profile.getEmail().indexOf('@')==-1)
+            throw new DaoException("Email provided is not valid");
+        if(profile.getEmail().equalsIgnoreCase(profileDao.read(profile.getId()).getEmail()))
+            throw new DaoException("Email address already exist in the database");
 
         // TODO test this service (not here though)
-
         profile.setPassword(passwordEncoder.encode(profile.getPassword()));
         return profile;
     }
