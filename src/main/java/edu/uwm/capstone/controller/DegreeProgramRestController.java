822cad71abea3862b197b8bfeb498c0270387d94
Julian Blecking <bleckin2@uwm.edu>
2019-11-14 00:19:26 -0600

    Finished DP-service, started DP-rest controller.

--- src/main/java/edu/uwm/capstone/controller/DegreeProgramRestController.java
+++ src/main/java/edu/uwm/capstone/controller/DegreeProgramRestController.java
@@ -1,7 +1,69 @@
 package edu.uwm.capstone.controller;
 
+import edu.uwm.capstone.model.DegreeProgram;
+import edu.uwm.capstone.service.DegreeProgramService;
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RestController
+@SuppressWarnings("squid:S1075") // suppress sonar warning about hard-coded URL path
 public class DegreeProgramRestController {
 
+    static final String DEGREE_PROGRAM_PATH = "/degreeProgram/";
+
+    private static final Logger LOG = LoggerFactory.getLogger(DegreeProgramRestController.class);
+
+    private final DegreeProgramService degreeProgramService;
+
+    @Autowired
+    public DegreeProgramRestController(DegreeProgramService dpService) {
+        this.degreeProgramService = dpService;
+    }
+
+    @ApiOperation(value = "Create Degree Program")
+    @PostMapping(value = DEGREE_PROGRAM_PATH)
+    public DegreeProgram create(@RequestBody DegreeProgram dp, @ApiIgnore HttpServletResponse response) throws IOException {
+        return RestControllerUtil.runCallable(() -> degreeProgramService.create(dp), response, LOG);
+    }
+
+    @ApiOperation(value = "Read Degree Program by ID")
+    @GetMapping(value = DEGREE_PROGRAM_PATH + "{degreeProgramId}")
+    public DegreeProgram readById(@PathVariable Long degreeProgramId, @ApiIgnore HttpServletResponse response) throws IOException {
+        return RestControllerUtil.runCallable(() -> degreeProgramService.read(degreeProgramId), response, LOG);
+    }
+
+    
+
+    /*
+    @ApiOperation(value = "Read All Form Definitions")
+    @GetMapping(value = FORM_DEF_PATH)
+//    @PreAuthorize("hasAuthority('TEST')") // TODO figure out how we can use the Authorities enum here
+    public List<FormDefinition> readAll() {
+        return formDefinitionService.readAll();
+    }
+
+
+    @ApiOperation(value = "Update Form Definition by ID")
+    @PutMapping(value = FORM_DEF_PATH + "{formDefId}")
+    public FormDefinition update(@PathVariable Long formDefId, @RequestBody FormDefinition formDefinition, @ApiIgnore HttpServletResponse response) throws IOException {
+        formDefinition.setId(formDefId);
+        return RestControllerUtil.runCallable(() -> formDefinitionService.update(formDefinition), response, LOG);
+    }
+
+
+    @ApiOperation(value = "Delete FormDefinition by ID")
+    @DeleteMapping(value = FORM_DEF_PATH + "{formDefId}")
+    public void deleteById(@PathVariable Long formDefId, @ApiIgnore HttpServletResponse response) throws IOException {
+        RestControllerUtil.runRunnable(() -> formDefinitionService.delete(formDefId), response, LOG);
+    }
+ */
 
 
 
--- src/main/java/edu/uwm/capstone/service/DegreeProgramService.java
+++ src/main/java/edu/uwm/capstone/service/DegreeProgramService.java
@@ -2,9 +2,14 @@ package edu.uwm.capstone.service;
 
 import edu.uwm.capstone.db.DegreeProgramDao;
 import edu.uwm.capstone.model.DegreeProgram;
+import edu.uwm.capstone.service.exception.EntityNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+
+import javax.swing.text.html.parser.Entity;
+import java.util.List;
 
 public class DegreeProgramService {
 
@@ -23,6 +28,51 @@ public class DegreeProgramService {
         return degreeProgramDao.create(dp);
     }
 
+    public DegreeProgram read(Long degreeProgramId) {
+        LOG.trace("Reading degree program {}", degreeProgramId);
 
+        DegreeProgram dp = degreeProgramDao.read(degreeProgramId);
 
+        if(dp == null) {
+            throw new EntityNotFoundException("Degree program with ID: " + degreeProgramId + " not found.");
+        }
+        return dp;
+    }
+
+    public List<DegreeProgram> readAll() {
+        return degreeProgramDao.readAll();
+    }
+
+    public DegreeProgram update(DegreeProgram dp) {
+        LOG.trace("Updating degree program {}", dp);
+
+        // check valid dp
+        DegreeProgram dpInDb = degreeProgramDao.read(dp.getId());
+
+        if(dpInDb == null) {
+            throw new EntityNotFoundException("Could not update degree program " + dp.getId() + " - record not found.");
+        }
+
+        dp.setCreatedDate(dpInDb.getCreatedDate());
+        return degreeProgramDao.update(dp);
+    }
+
+    public void delete(Long degreeProgramId) {
+        LOG.trace("Deleting degree program {}", degreeProgramId);
+        if (degreeProgramDao.read(degreeProgramId) == null) {
+            throw new EntityNotFoundException("Could not delete degree program " + degreeProgramId + " - record not found.");
+        }
+        degreeProgramDao.delete(degreeProgramId);
+    }
+
+    public void checkValidDegreeProgram(DegreeProgram dp, boolean checkNullId) {
+        Assert.notNull(dp, "Degree program cannot be null.");
+        if(checkNullId) {
+            Assert.isNull(dp.getId(), "Degree program ID should be null.");
+        }
+        Assert.notNull(dp.getName(), "Degree program name cannot be null.");
+        Assert.notNull(dp.getDescription(), "Degree program description cannot be null.");
+
+        //TODO: check that degree program's connected degree program states aren't null
+    }
 }
