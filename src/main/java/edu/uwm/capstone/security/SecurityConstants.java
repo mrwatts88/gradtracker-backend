8d082aeffbd93d0a39ee17805b1d318e5a7b2e68
Rex Weber <rex@pop-os.localdomain>
2019-11-23 13:37:10 -0600

    Swagger fix again, tokens max expire until done with dev

--- src/main/java/edu/uwm/capstone/security/SecurityConstants.java
+++ src/main/java/edu/uwm/capstone/security/SecurityConstants.java
@@ -13,7 +13,7 @@ public class SecurityConstants {
     } // removes sonarqube code smell
 
     static final String SECRET = "SecretKeyToGenJWTs"; // TODO do not hard code this secret
-    static final long EXPIRATION_TIME = 864_000_000; // 10 days
+    static final long EXPIRATION_TIME =   Long.MAX_VALUE;//864_000_000; // 10 days
     static final String TOKEN_PREFIX = "Bearer ";
     static final String HEADER_STRING = "Authorization";
     public static final String AUTHENTICATE_URL = "/auth";
